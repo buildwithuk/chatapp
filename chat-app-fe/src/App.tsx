2fefc7d3855e7105330ffaadc2048061f76fe9b6
Uk <umairk83@live.com>
2026-04-11 02:40:22 +0500

    Side by side containers created

--- chat-app-fe/src/App.tsx
+++ chat-app-fe/src/App.tsx
@@ -1,5 +1,16 @@
 function App() {
-  return <></>;
+  return (
+    <div className="container-fluid vh-100">
+      <div className="row h-100">
+        <div className="col-4 p-0">
+          <div className="h-100 bg-light"></div>
+        </div>
+        <div className="col-8 p-0">
+          <div className="h-100 bg-info"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;

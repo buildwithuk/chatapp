79b846e9f48a225145ef10a4a95cc3d062f710b3
Uk <umairk83@live.com>
2026-04-12 01:17:06 +0500

    Created search box

--- chat-app-fe/src/App.tsx
+++ chat-app-fe/src/App.tsx
@@ -1,10 +1,32 @@
+import { List, PersonCircle, Search } from "react-bootstrap-icons";
+
 function App() {
   return (
     <div className="container-fluid vh-100">
       <div className="row h-100">
+        {/*Contacts chat*/}
         <div className="col-4 p-0">
-          <div className="h-100 bg-light"></div>
+          <div className="h-100 bg-light m-2">
+            {/*Top bar*/}
+            <div className="border d-flex align-items-center gap-3 p-2 ">
+              <PersonCircle className="fs-3"></PersonCircle>
+
+              <div className="input-group">
+                <span className="input-group-text">
+                  <Search></Search>
+                </span>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Search..."
+                />
+              </div>
+              <List className="fs-3"></List>
+            </div>
+          </div>
         </div>
+
+        {/*Messages chat*/}
         <div className="col-8 p-0">
           <div className="h-100 bg-info"></div>
         </div>

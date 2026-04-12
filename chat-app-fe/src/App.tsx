ec71de2f57b8aea44bba52cba15e4a8359af9550
Uk <umairk83@live.com>
2026-04-12 22:31:52 +0500

    Created list for the nearby contacts

--- chat-app-fe/src/App.tsx
+++ chat-app-fe/src/App.tsx
@@ -1,4 +1,11 @@
-import { List, PersonCircle, Search, Image } from "react-bootstrap-icons";
+import {
+  List,
+  PersonCircle,
+  Search,
+  Image,
+  CursorFill,
+  PersonFill,
+} from "react-bootstrap-icons";
 
 function App() {
   return (
@@ -22,6 +29,59 @@ function App() {
                 />
               </div>
               <List className="fs-3"></List>
+            </div>
+            <div className="d-flex flex-column px-4 h-50">
+              <div className="d-flex text-primary gap-3 align-items-center py-4 mb-2">
+                <CursorFill className="fs-4" />
+                <p className="mb-0 lead">Nearby Contacts</p>
+              </div>
+              <div
+                className="d-flex flex-column gap-3 overflow-auto"
+                style={{ height: "50%" }}
+              >
+                <div className="d-flex align-items-center gap-3">
+                  <button type="button" className="btn align-items-center w-100 text-start">
+                    <PersonFill className="fs-5" />
+                    <span className="text-secondary mx-3">SnazzySloth</span>
+                  </button>
+                </div>
+                <div className="d-flex align-items-center gap-3">
+                  <button type="button" className="btn align-items-center w-100 text-start">
+                    <PersonFill className="fs-5" />
+                    <span className="text-secondary mx-3">TacoTuesday</span>
+                  </button>
+                </div>
+                <div className="d-flex align-items-center gap-3">
+                  <button type="button" className="btn align-items-center w-100 text-start">
+                    <PersonFill className="fs-5" />
+                    <span className="text-secondary mx-3">CosmicCorgi</span>
+                  </button>
+                </div>
+                <div className="d-flex align-items-center gap-3">
+                  <button type="button" className="btn align-items-center w-100 text-start">
+                    <PersonFill className="fs-5" />
+                    <span className="text-secondary mx-3">JamminJelly</span>
+                  </button>
+                </div>
+                <div className="d-flex align-items-center gap-3">
+                  <button type="button" className="btn align-items-center w-100 text-start">
+                    <PersonFill className="fs-5" />
+                    <span className="text-secondary mx-3">ZenZebra</span>
+                  </button>
+                </div>
+                <div className="d-flex align-items-center gap-3">
+                  <button type="button" className="btn align-items-center w-100 text-start">
+                    <PersonFill className="fs-5" />
+                    <span className="text-secondary mx-3">MellowMango</span>
+                  </button>
+                </div>
+                <div className="d-flex align-items-center gap-3">
+                  <button type="button" className="btn align-items-center w-100 text-start">
+                    <PersonFill className="fs-5" />
+                    <span className="text-secondary mx-3">SassySquid</span>
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
